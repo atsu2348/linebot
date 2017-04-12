555e6b11f5dca36418e1e5e4d9045b1fc045071e
u276161 <atsu2348@gmail.com>
2017-04-13 02:00:35 +0900

    test commit3

--- callback/index.php
+++ callback/index.php
@@ -11,7 +11,7 @@ $replyToken = $jsonObj->{"events"}[0]->{"replyToken"};
 //相手のuserId
 $userId = $jsonObj->{"events"}[0]->{"source"}->{"userId"};
 $now_day = date('Y-m-d');
-$now_time = date('H:i.s');
+$now_time = date('H時i分s秒');
 //メッセージ以外のときは何も返さず終了
 if($type != "text"){
 	 exit;
